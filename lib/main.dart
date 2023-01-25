1cb3c0282b9281f4a976adbcb79b2251fd6a0c96
Mateus Lima <70021830+MateusLim4@users.noreply.github.com>
2023-01-25 12:46:41 -0300

    Added: Provider config

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_app/models/product_list.dart';
 import 'package:shop_app/pages/product_detail_page.dart';
 import 'package:shop_app/pages/products_overview_page.dart';
 import 'package:shop_app/routes/routes.dart';
@@ -15,15 +17,20 @@ class ShopApp extends StatelessWidget {
 
   @override
   build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "ShopApp",
-      theme: ThemeData(
-          primarySwatch: AppTheme.colors.primarySwatch,
-          backgroundColor: AppTheme.colors.background,
-          fontFamily: GoogleFonts.lato().fontFamily),
-      home: ProductsOverviewPage(),
-      routes: {AppRoutes.productDetail: (context) => const ProductDetailPage()},
+    return ChangeNotifierProvider(
+      create: (_) => ProductList(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: "ShopApp",
+        theme: ThemeData(
+            primarySwatch: AppTheme.colors.primarySwatch,
+            backgroundColor: AppTheme.colors.background,
+            fontFamily: GoogleFonts.lato().fontFamily),
+        home: const ProductsOverviewPage(),
+        routes: {
+          AppRoutes.productDetail: (context) => const ProductDetailPage()
+        },
+      ),
     );
   }
 }
--- lib/pages/products_overview_page.dart
+++ lib/pages/products_overview_page.dart
@@ -1,13 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:shop_app/data/dummy_data.dart';
-import 'package:shop_app/theme/app_theme.dart';
-import 'package:shop_app/widgets/product_item.dart';
-
-import '../models/product.dart';
+import '../widgets/product_grid.dart';
 
 class ProductsOverviewPage extends StatelessWidget {
-  final List<Product> loadedProducts = dummyProducts;
-  ProductsOverviewPage({super.key});
+  const ProductsOverviewPage({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -15,17 +10,7 @@ class ProductsOverviewPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Minha Loja"),
       ),
-      body: GridView.builder(
-        padding: const EdgeInsets.all(10),
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 10,
-        ),
-        itemCount: loadedProducts.length,
-        itemBuilder: (ctx, i) => ProductItem(product: loadedProducts[i]),
-      ),
+      body: const ProductGrid(),
     );
   }
 }
