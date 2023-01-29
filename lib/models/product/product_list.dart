ddda791d3dc5addc395a425e761c314708004342
Mateus Lima <70021830+MateusLim4@users.noreply.github.com>
2023-01-29 15:12:08 -0300

    Http firebase add product

--- lib/models/product/product_list.dart
+++ lib/models/product/product_list.dart
@@ -1,33 +1,19 @@
+import 'dart:convert';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:shop_app/data/dummy_data.dart';
 import 'package:shop_app/models/product/product.dart';
 
 class ProductList with ChangeNotifier {
+  final _baseUrl = "URL";
   final List<Product> _items = dummyProducts;
 
   List<Product> get items => [..._items];
 
   List<Product> get favoriteItems =>
       _items.where((product) => product.isFavorite).toList();
-
-  void addProduct(Product product) {
-    _items.add(product);
-    notifyListeners(); // Notifica todos widgets interessados após adicionar um widget
-  }
-
-  void updateProduct(Product product) {
-    int index = _items.indexWhere((p) => p.id == product.id);
-    if (index >= 0) {
-      _items[index] = product;
-      notifyListeners();
-    }
-  }
-
-  int get countItens {
-    return _items.length;
-  }
 
   void addProductFromData(Map<String, dynamic> data) {
     bool hasId = data['id'] != null;
@@ -45,6 +31,49 @@ class ProductList with ChangeNotifier {
     } else {
       addProduct(product);
     }
+  }
+
+  void addProduct(Product product) {
+    final future = http.post(
+      Uri.parse("$_baseUrl/products.json"),
+      body: jsonEncode(
+        {
+          "name": product.title,
+          "price": product.price,
+          "description": product.description,
+          "imageUrl": product.imageUrl,
+          "isFavorite": product.isFavorite,
+        },
+      ),
+    );
+
+    future.then(
+      (response) {
+        final id = jsonDecode(response.body)["name"];
+        _items.add(
+          Product(
+              id: id,
+              title: product.title,
+              description: product.description,
+              price: product.price,
+              imageUrl: product.imageUrl,
+              isFavorite: product.isFavorite),
+        );
+        notifyListeners(); // Notifica todos widgets interessados após adicionar um widget
+      },
+    );
+  }
+
+  void updateProduct(Product product) {
+    int index = _items.indexWhere((p) => p.id == product.id);
+    if (index >= 0) {
+      _items[index] = product;
+      notifyListeners();
+    }
+  }
+
+  int get countItens {
+    return _items.length;
   }
 
   void removeProduct(Product product) {
